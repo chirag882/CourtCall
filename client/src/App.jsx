605ea3f9d6f2d9aad490d88133d4e37c4761a814
Chirag Ailani <chiragailani35@gmail.com>
2023-04-17 12:30:55 +0530

    Started Creating contact and home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import Home from "./pages/home/Home";
+import Contact from "./pages/contact/Contact";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/contact" element={<Contact/>}/> 
+    </Routes>
+    
+  );
 }
 
 export default App;
