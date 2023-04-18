84da614cccd45a4218a658fb0d079ae88b92c8f8
Gaurav701a <gaurav30.sahu@gmail.com>
2023-04-18 23:55:50 +0530

    nav updation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import Home from "./pages/home/Home";
 import Contact from "./pages/contact/Contact";
+import About from "./pages/about/About";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home/>}/>
-      <Route path="/contact" element={<Contact/>}/> 
+      <Route path="/contact" element={<Contact/>}/>
+      <Route path="/about" element={<About/>}/>
     </Routes>
     
   );
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,51 +1,79 @@
 import React from "react";
-import {
-  RouterProvider,
-  Route,
-  Link,
-} from "react-router-dom";
+import { RouterProvider, Route, Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <Link to="#" className="navbar-brand" >
-            Navbar
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link to = "/" className="nav-link active" aria-current="page">
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to={"/contact"} className="nav-link" >
-                  Contact
-                </Link>
-              </li>
-            </ul>
-            <form className="d-flex">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
+    <>
+      <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <div class="container-fluid">
+            <Link to="#" className="navbar-brand">
+              Navbar
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <Link to="/" className="nav-link active" aria-current="page">
+                    Home
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={"/contact"} className="nav-link">
+                    Contact
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={"/about"} className="nav-link">
+                    About us
+                  </Link>
+                </li>
+              </ul>
+              <form className="d-flex">
+                <input
+                  className="form-control me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+                <button className="btn btn-outline-success" type="submit">
+                  Search
+                </button>
+              </form>
+            </div>
           </div>
-        </div>
-      </nav>
-    </div>
+        </nav>
+      </div>
+      <div class="jumbotron">
+        <h1 class="display-4">Hello, world!</h1>
+        <p class="lead">
+          This is a simple hero unit, a simple jumbotron-style component for
+          calling extra attention to featured content or information.
+        </p>
+        <hr class="my-4" />
+        <p>
+          It uses utility classes for typography and spacing to space content
+          out within the larger container.
+        </p>
+        <p class="lead">
+          <a class="btn btn-primary btn-lg" href="#" role="button">
+            Learn more
+          </a>
+        </p>
+      </div>
+    </>
   );
 }
